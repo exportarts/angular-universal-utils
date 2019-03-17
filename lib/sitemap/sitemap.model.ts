262c806a366898fb40a2fee2437306dedb753bf2
Enzo Volkmann <enzo@exportarts.io>
2019-03-17 21:45:37 +0100

    Added more options to sitemap configuration

--- lib/sitemap/sitemap.model.ts
+++ lib/sitemap/sitemap.model.ts
@@ -4,4 +4,30 @@ export interface SitemapConfig {
      * Defaults to 'sitemap.xml'
      */
     filename?: string;
+    routes: RouteWithMetadata[];
+    xslUrl?: string;
+}
+
+export interface RouteWithMetadata {
+    url: string;
+    changeFreq?: string;
+    lastModISO?: string;
+    priority?: number;
+    androidLink?: string;
+    mobile?: boolean;
+    img?: Img[];
+    links?: Link[];
+}
+
+export interface Img {
+    url: string;
+    caption?: string;
+    title?: string;
+    geoLocation?: string;
+    license?: string;
+}
+
+export interface Link {
+    lang: string;
+    url: string;
 }
